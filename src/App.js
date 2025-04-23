8a59de730b89a5abe67d901ebf3996bdfaab5520
Thiri Myat Thu <thitimyatthu.y2k3@gmail.com>
2025-04-23 09:45:02 +0700

    Intialize the hooks react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-// src/App.js
-import React, { useState } from 'react';
+import { useState } from 'react'; 
 
-function App() {
-  const [inputText, setText] = useState('initialize'); 
-
-  function handleChange(e) { 
-    setText(e.target.value); 
-  } 
+export default function RegisterForm() { 
+  const [form, setForm] = useState({ 
+    firstName: 'Luke', 
+    lastName: 'Jones', 
+    email: 'lukeJones@sculpture.com', 
+  }); 
 
   return ( 
     <> 
-      <input value={inputText} onChange={handleChange} /> 
-      <p>You typed: {inputText}</p> 
-      <button onClick={() => setText('hello')}> 
-        Reset 
-      </button> 
+      <label> 
+        First name: 
+        <input 
+          value={form.firstName} 
+          onChange={e => { 
+            setForm({ 
+              ...form, 
+              firstName: e.target.value 
+            }); 
+          }} 
+        /> 
+      </label> 
+      <label> 
+        Last name: 
+        <input 
+          value={form.lastName} 
+          onChange={e => { 
+            setForm({ 
+              ...form, 
+              lastName: e.target.value 
+            }); 
+          }} 
+        /> 
+      </label> 
+      <label> 
+        Email: 
+        <input 
+          value={form.email} 
+          onChange={e => { 
+            setForm({ 
+              ...form, 
+              email: e.target.value 
+            }); 
+          }} 
+        /> 
+      </label> 
+      <p> 
+        {form.firstName}{' '} 
+        {form.lastName}{' '} 
+        ({form.email})
+      </p> 
     </> 
   ); 
-}
-
-export default App;
-
+} 
